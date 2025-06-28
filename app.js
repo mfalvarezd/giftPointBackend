8c9fdcd087a51f41958bfdca297dd010fc42d8ad
Moises Alvarez <mfalvare@espol.edu.ec>
2025-06-28 13:22:02 -0500

    Add request logger middleware
    Introduced a logger middleware that logs HTTP method, URL, and response time for each request. Enabled the logger in the Express app to improve request tracing and debugging.

--- app.js
+++ app.js
@@ -3,11 +3,11 @@ const express = require('express');
 const app = express();
  const authRoutes = require('./routes/authRoutes');
  const giftCardRoutes = require('./routes/giftCardRoutes');
- //const logger = require('./middleware/logger');
+ const logger = require('./middleware/logger');
 
  app.use(express.json());
 
- //app.use(logger);
+ app.use(logger);
 
  app.use('/auth', authRoutes);
  app.use('/giftcards', giftCardRoutes);
